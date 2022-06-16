1ccaed208507c63827bf5ffb7ac328e823cb5a0b
ahmet.thiam <ahmet.thiam@uvs.edu.sn>
2022-06-16 17:17:31 +0000

    maj

--- src/main/java/com/iso4digit/insuranceapi/services/DataService.java
+++ src/main/java/com/iso4digit/insuranceapi/services/DataService.java
@@ -12,6 +12,8 @@ import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import javax.management.loading.PrivateClassLoader;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,32 +41,47 @@ public class DataService {
     @Autowired
     private BrancheRepository brancheRepository;
 
-    public Map<String, Data> getData(Map<String, String> qp) {
+    public List<Data> getData(Map<String, String> qp) {
         CaseType[] cases = caseRepository.getCases();
 
         Map<String, Data> datas = new HashMap<>();
         for (CaseType c : cases) {
             String key = c.getCedants_id().getOid() + c.getSlipes_prime_id().getOid() + c.getBranch();
 
-            if (datas.get(key) == null) {
-                BigDecimal prime = BigDecimal.valueOf(c.getPremium_ht()*0.36);
-                datas.put(key,
-                        this.createNewDataInstance(
-                                slipRepository.findSlipeById(c.getSlipes_prime_id().getOid()),
-                                qp.get("branche"),
-                                prime
-                                ));
-            } else {
-                BigDecimal prime = datas.get(key).getCalculatedREC();
-                prime = BigDecimal.valueOf(c.getPremium_ht()*0.36).add(prime);
-                datas.get(key).setCalculatedREC(prime);
+            if (this.match(c, qp)) {
+                if (datas.get(key) == null) {
+                    BigDecimal prime = BigDecimal.valueOf(c.getPremium_ht() * 0.36);
+                    datas.put(key,
+                            this.createNewDataInstance(
+                                    slipRepository.findSlipeById(c.getSlipes_prime_id().getOid()),
+                                    qp.get("branche"),
+                                    prime));
+                } else {
+                    BigDecimal prime = datas.get(key).getCalculatedREC();
+                    prime = BigDecimal.valueOf(c.getPremium_ht() * 0.36).add(prime);
+                    datas.get(key).setCalculatedREC(prime);
+                }
             }
-            
+
         }
-        return datas;
+        return new ArrayList<Data>(datas.values());
 
-        
+    }
 
+    private boolean match(CaseType c, Map<String, String> qp) {
+        return Objects.equals(cedantRepository.getCedantRegionId(c.getCedants_id()), qp.get("region_id"))
+                && Objects.equals(cedantRepository.getCedantCountryId(c.getCedants_id()), qp.get("country_id"))
+                && Objects.equals(cedantRepository.getCedantGroupId(c.getCedants_id()), qp.get("group_id"))
+                && Objects.equals(cedantRepository.getCedantTypeId(c.getCedants_id()), qp.get("cedant_type_id"))
+                && Objects.equals(cedantRepository.getCedantId(c.getCedants_id()), qp.get("cedant_id"))
+                && Objects.equals(slipRepository.getValidationStatus(c.getSlipes_prime_id()),
+                        qp.get("validation_status"))
+                && Objects.equals(slipRepository.getConfirmationStatus(c.getSlipes_prime_id()),
+                        qp.get("confirmation_status"))
+                && Objects.equals(this.getDate(slipRepository.getPublishedDate(c.getSlipes_prime_id())),
+                        qp.get("published_date"))
+                && Objects.equals(slipRepository.getEditedPeriode(c.getSlipes_prime_id()), qp.get("edited_period"))
+                && Objects.equals(c.getBranch(), qp.get("branche"));
     }
 
     private Data createNewDataInstance(Slip s, String branche, BigDecimal prime) {
